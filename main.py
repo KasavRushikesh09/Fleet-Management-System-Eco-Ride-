90a9bc170964c2f6d3c454e68b328ce4f43506d9
Rushikesh Kasav <rishikeshkasav2003@gmail.com>
2026-01-04 23:30:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class EcoRideMain:
 
 if __name__ == "__main__":
     EcoRideMain.greet()
-    e = EcoRideMain(123,"ather",90,100)
+    e = ElectricScooter(123,"ather",90,100)
     print(e.calculate_trip_cost(20))
     c = ElectricCar(1,"bmw",90,4)
     print(c.calculate_trip_cost(30))
