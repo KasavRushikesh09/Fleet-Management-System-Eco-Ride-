73015062aaded2bdffdb4cc30f394877167842e8
Rushikesh Kasav <rishikeshkasav2003@gmail.com>
2026-01-04 23:38:51 +0530

    Fix super() call in ElectricCar constructor

--- ElectricCar.py
+++ ElectricCar.py
@@ -2,7 +2,7 @@ from vehicle import Vehicle
 
 class ElectricCar(Vehicle):
     def __init__(self,vehicle_id,model,battery_percentage,seating_capacity):
-        super().__init__(self,vehicle_id,model,battery_percentage)
+        super().__init__(vehicle_id,model,battery_percentage)
         if seating_capacity>0:
             self.seating_capacity = seating_capacity
         else:
